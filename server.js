44a0a7e6bd03e7e371536706b99902b9eab79a63
NeverGiveUpForLife32 <clflem687@yahoo.com>
2024-01-09 17:51:35 -0500

    Completed the set up to start captains_log_2

---
+++ server.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const jsxEngine = require("jsx-view-engine");
+const Log = require("./models/logs");
+const PORT = process.env.PORT || 3002;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "jsx");
+app.engine("jsx", jsxEngine());
+
+mongoose.connect(process.env.MONGO_URI);
+mongoose.connection.once("open", () => {
+  console.log("connected to mongodb");
+});
+
+// INDUCES
+
+// INDEX
+// list all log
+
+// NEW
+// show the user a form to fill out to create a log
+app.get("/logs/new", (req, res) => {
+  res.render("logs/New");
+});
+
+// DELETE
+// backend only functionality that is used to delete a log
+
+// UPDATE
+// backend only functionality that is used to update a log
+
+// CREATE
+// backend only functionality that is used to create a log
+
+// EDIT
+// show you a form that lets you edit the log
+
+// SHOW
+// Shows you 1 individual log
+
+app.listen(PORT, () => {
+  console.log(`Ayo the Port at ${PORT} is lit `);
+});
